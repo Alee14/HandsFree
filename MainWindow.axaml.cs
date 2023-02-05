c1ae32ee04460442046d5276bb57726d6d1d6e92
Andrew Lee <alee14498@protonmail.com>
2023-02-05 18:32:19 -0500

    Inital commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HandsFree;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HandsFree
+A HTPC launcher built for a soon to be immutable distro
